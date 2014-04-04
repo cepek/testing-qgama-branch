497527c59b837edfec6e2451c068e7935e6bd6b7
Ales Cepek <cepek@gnu.org>
2014-04-04 21:04:10 +0200

    added copyright notice to all source files

--- gama-q2/dbfunctions.cpp
+++ gama-q2/dbfunctions.cpp
@@ -1,3 +1,22 @@
+/*  Qt based GUI for GNU Gama -- adjustment of geodetic networks
+    Copyright (C) 2014  Ales Cepek <cepek@gnu.org>
+
+    This file is a part of GNU Gama.
+
+    GNU Gama is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GNU Gama is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "dbfunctions.h"
 #include "constants.h"
 #include "selectconfiguration.h"
