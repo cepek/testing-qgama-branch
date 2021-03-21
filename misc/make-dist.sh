f0908bebb2a34da7ed923f35368c1ee6ad8be9e0
Ales Cepek <cepek@gnu.org>
2021-03-21 15:38:06 +0100

    updated script misc/make-dist.sh

--- misc/make-dist.sh
+++ misc/make-dist.sh
@@ -7,27 +7,11 @@ if [ "$1" = "" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     exit 1;
 fi
 
-# echo
-# git fetch --all
-
-# echo
-# git show-ref --tags
-#
-# # turn the detached message off
-# # git config --global advice.detachedHead false
-# echo
-# git checkout tags/$1
-# git describe --tags --abbrev=0
-
 
 # run from top git directory
 #
-git archive --format=tar.gz --prefix=$1/ --output $1.tar.gz $1
+git archive --worktree-attributes --format=tar.gz --prefix=$1/ \
+    --output $1.tar.gz $1
 
-# git archive --format=zip    --prefix=$1/ --output $1.zip    $1
-#
-# echo
-# ls -l $1.tar.gz $1.zip
-#
-# echo
-# git checkout master
+# git archive --worktree-attributes --format=zip    --prefix=$1/ \
+#     --output $1.zip    $1
