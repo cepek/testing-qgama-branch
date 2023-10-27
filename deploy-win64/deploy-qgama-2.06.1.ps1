82f3a516e4f2068a03ae331d7e8734da5e94bd65
Ales Cepek <cepek@gnu.org>
2023-10-27 21:08:53 +0200

    Add Get-FileHash to deploy-qgama-2.06.1.ps1

--- deploy-win64/deploy-qgama-2.06.1.ps1
+++ deploy-win64/deploy-qgama-2.06.1.ps1
@@ -1,8 +1,5 @@
-#  
-# run this file from the build directory
 #
-# cmake ..
-# cmake --build . --config Release
+# run this file from the qgama build directory
 #
 
 if ($args.Count -ne 0)
@@ -13,36 +10,42 @@ if ($args.Count -ne 0)
     exit
 }
 
-$ver="2.06.1"     # release version
-$rel=""           # optional release directory  
+$ver="2.06.X"     # release version
+#$rel=""          # optional release directory
 $windeployqt="C:\Qt\6.2.2\msvc2019_64\bin\windeployqt.exe"
+
+Write-Host
 
 $errs=0
 if (-Not (Test-Path -Path $windeployqt)) {
-    Write-Host "`nProgram " $windeployqt.ToString() " not found" 
+    Write-Host "Program " $windeployqt.ToString() " not found"
     $errs+=1
 }
 
 if (Test-Path -Path qgama-$ver) {
-    Write-Host "`nDirectory qgama-$ver already exists"
+    Write-Host "Directory qgama-$ver already exists"
     $errs+=1
 }
 
 if (Test-Path -Path qgama-$ver.zip) {
-    Write-Host "`nFile qgama-$ver.zip already exists"
+    Write-Host "File qgama-$ver.zip already exists"
     $errs+=1
 }
 
 if ($errs) {
-    Write-Host "`n"
+    Write-Host "`nNothing was done`n"
     exit
 }
 
-New-Item -ItemType Directory -Path qgama-$ver
-New-Item -ItemType Directory -Path qgama-$ver/bin
+New-Item -ItemType Directory -Path qgama-$ver      | Out-Null
+New-Item -ItemType Directory -Path qgama-$ver/bin  | Out-Null
 
 Copy-Item qgama.exe      -Destination  qgama-$ver
 Copy-Item gama$rel\*.exe -Destination  qgama-$ver/bin
 
 Invoke-Expression "$windeployqt qgama-$ver\qgama.exe"
 Compress-Archive  -Path qgama-$ver qgama-$ver.zip
+
+# pick your choice
+Get-FileHash -Algorithm sha256 qgama-$ver.zip | Out-File .\qgama-$ver.sha256
+Get-FileHash -Algorithm md5    qgama-$ver.zip | Out-File .\qgama-$ver.md5
